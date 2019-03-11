d53b6b453022d4a8ca3e8369b20be0444bcbe96d
Aluno <Aluno@SN-279232>
2019-03-11 20:24:06 -0300

    *Tela de cadastro de agenda concluído *Data não está funcionando sem ser setada direto em AgendaDao
    (Ver com o professor sobre a data)

--- src/ConnectionFactory/ConnectionFactory.java
+++ src/ConnectionFactory/ConnectionFactory.java
@@ -7,13 +7,13 @@ import java.sql.Connection;
 
 public class ConnectionFactory {
 
-    public static Connection main(String[] args) {
+    public Connection getConnection() {
         try {
             return DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1/sistema_gestao", "root", "");
-        } catch (SQLException e){
-            throw new RuntimeException (e);
-        }       
+                    "jdbc:mysql://127.0.0.1/sistema_gestao", "root", "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     
 }
